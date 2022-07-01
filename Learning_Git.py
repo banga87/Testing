47c388c3818c79942838d6f609f5f06cf47d0598
banga87 <angusroymcdonald@gmail.com>
2022-07-01 15:05:28 +1000

    Adding my name to the code

--- Learning_Git.py
+++ Learning_Git.py
@@ -2,3 +2,5 @@ print("I'm learing how to use Git and GitHub")
 print("This will be the first commit to the GitHub depository")
 
 print("This is a new line of code in the new branch")
+
+name = "Angus"
